282fa6d8a065242109d4b922e6e34a69be952576
Ashraf Ali <ashrafali379@gmail.com>
2022-09-18 00:42:39 +0100

    added parse_image implementation

--- interface.py
+++ interface.py
@@ -1,7 +1,17 @@
+import io
 from os import environ
+from PIL import Image
 
 from google.cloud import translate
+from google.cloud import vision
 
+# Convert Image object (PIL) to bytes - so we can access the image in-memory
+#   and don't need to store the Image object in a file.
+def pilToBytes(pilImage):
+    buffer = io.BytesIO()
+    pilImage.save(buffer, format="PNG")
+    return buffer.getvalue()
+    
 class ImageToOutput:
     def __init__(self):
         self.translate_client = translate.TranslationServiceClient()
@@ -21,8 +31,20 @@ class ImageToOutput:
         
         return [translation.translated_text for translation in response.translations]
 
-    def __parse_image(self, pil_image):
-        pass
+    def parse_image(self, pil_image):
+        pngBytes = pilToBytes(pil_image)
+
+        # TEMP: Double check we got the bytes for the PNG
+        print(pngBytes[:20])
+
+        # GCP code
+        client = vision.ImageAnnotatorClient()
+
+        gcpImage = vision.Image(content=pngBytes)
+        response = client.text_detection(image=gcpImage)
+        boundedTexts = response.text_annotations
+
+        return boundedTexts
     
     def setup(self, image):
         pass
@@ -33,3 +55,4 @@ class ImageToOutput:
         pass
 
 output = ImageToOutput()
+print(output.parse_image(Image.open("assets/sample_text_1.png")))
