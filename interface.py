0bd086cd0f6b43cf86543f869bf9282fb86759c9
Samantha Zhan <samanthazhanjiaqi@gmail.com>
2022-09-18 02:28:10 -0400

    test

--- interface.py
+++ interface.py
@@ -5,12 +5,12 @@ from email.mime import image
 import io
 from os import environ
 from PIL import Image
+from utils import rectangle, word_rectangle_pair
 
-import imageToText
 
 from google.cloud import translate
 from google.cloud import vision
-from PyDictionary import PyDictionary
+# from PyDictionary import PyDictionary
 
 import wordToSentence
 
@@ -27,8 +27,8 @@ class ImageToOutput:
 
         self.full_text = ""
 
-    def __get_definition(self, word):
-        return PyDictionary.meaning(word)
+    # def __get_definition(self, word):
+    #     return PyDictionary.meaning(word)
 
     # Takes list of text, returns list of translations
     def __get_translation(self, text):
@@ -43,9 +43,7 @@ class ImageToOutput:
         return [translation.translated_text for translation in response.translations]
 
     def setup(self, pil_image):
-        full_text_and_lines_array = imageToText.parse_image(
-            self.vision_client, pil_image=pil_image
-        )
+        full_text_and_lines_array = self.parse_image(pil_image=pil_image)
         self.full_text = full_text_and_lines_array[0]
         self.word_areas = full_text_and_lines_array[1]
 
@@ -85,24 +83,105 @@ class ImageToOutput:
 
     def process(self, x, y, process_type="translate"):
         word, column, row = self.find_word(x, y)
-        if process_type == "definition":
-            processedWord = self.__get_definition(word)
-        else:
-            processedWord = self.__get_translation(word)[0]
+        # if process_type == "definition":
+        #     processedWord = self.__get_definition(word)
+        # else:
+        print("non-translated is: "+word)
+        processedWord = self.__get_translation(word)[0]
         return processedWord, column, row
 
+    sampleImage = Image.open("assets/sample_text_3.png")
 
-# (DAVID) TESTING:
+    
+
+
+    # Input: Image object (PIL) to be passed in from Samantha's app.
+    # Output: [string of all text in the image, 2D array containing wordRectanglePairs]
+    def parse_image(self, pil_image=sampleImage):
+        client = self.vision_client
+        # Convert Image object (PIL) to bytes - so we can access the image in-memory
+        #   and don't need to store the Image object in a file.
+        def pil_to_bytes(pil_image):
+            buffer = io.BytesIO()
+            pil_image.save(buffer, format="PNG")
+            return buffer.getvalue()
+
+        pngBytes = pil_to_bytes(pil_image)
+
+        # GCP code
+        gcpImage = vision.Image(content=pngBytes)
+        response = client.text_detection(image=gcpImage)
+
+        if response.error.message:
+            raise Exception(
+                "{}\nFor more info on error messages, check: "
+                "https://cloud.google.com/apis/design/errors".format(response.error.message)
+            )
+
+        texts = response.text_annotations
+
+        # Grab all text from the image
+        full_text = response.text_annotations[0].description
+        texts.pop(0)
+
+        # Put each individual word into data structure
+        lines_array = []  # array of array of word-rectangle pairs, sorted by y-coord.
+        word_rectangle_pairs = []  # array of word-rectangle pairs, sorted by x-coord.
+
+        for textObj in texts:
+            word = textObj.description
+            rect = rectangle.Rectangle(
+                textObj.bounding_poly.vertices[0], textObj.bounding_poly.vertices[2]
+            )
+
+            if word.isalpha():
+                if word_rectangle_pairs and not rect.sameRectangleY(
+                    word_rectangle_pairs[0].rect
+                ):
+                    lines_array.append(word_rectangle_pairs)
+                    word_rectangle_pairs = []
+
+                word_rectangle_pairs.append(
+                    word_rectangle_pair.WordRectanglePair(word, rect, self.__get_translation(word)[0])
+                )
+
+        # # Testing correctness
+        # for line in lines_array:
+        #     print("--------------------")
+        #     for wordRectanglePair in line:
+        #         # print("Word: {}".format(wordRectanglePair.word))
+        #         print(
+        #             "Word: {}\nMinX: {} MaxX: {} MinY: {} MaxY: {}".format(
+        #                 wordRectanglePair.word,
+        #                 wordRectanglePair.rect.minX,
+        #                 wordRectanglePair.rect.maxX,
+        #                 wordRectanglePair.rect.minY,
+        #                 wordRectanglePair.rect.maxY,
+        #             )
+        #         )
+
+        return [full_text, lines_array]
+
+    # (DAVID) TESTING:
 thing = ImageToOutput()
 thing.setup(Image.open("assets/sample_text_3.png"))
+# thing.word_areas
+# len(self.word_areas)
+for i in range(len(thing.word_areas)):
+    for j in range(len(thing.word_areas[0])):
+        print(thing.word_areas[i][j])
+
+
 processedWord, column, row = thing.process(211, 320)
 print(processedWord)
+    # processedWord2, column2, row2 = thing.process(300, 100)
+    # print(processedWord2)
 
 
 # Testing:
 # output = ImageToOutput()
 # print(output.get_defintion("moist"))
 
-print(wordToSentence.wordToSentence("", column, row, thing.word_areas))
+# print(wordToSentence.wordToSentence("", column, row, thing.word_areas))
 # print("-----")
 # print(thing.full_text)
